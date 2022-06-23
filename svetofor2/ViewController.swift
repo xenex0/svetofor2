15bc80b3f2521a218f3462fc81197bac15d9c046
xenex0 <tarjafan09@gmail.com>
2022-06-23 11:06:02 +0300

    Initial Commit

---
+++ svetofor2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  svetofor2
+//
+//  Created by Дмитрий Абдуллаев on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
